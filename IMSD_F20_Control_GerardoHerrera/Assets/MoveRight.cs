102ae22364b0636cf5bc1e73ed3a5cef6b704b51
Gerardo Herrera <gerardohe17@gmail.com>
2020-09-08 16:04:45 -0700

    MoveRight

---
+++ IMSD_F20_Control_GerardoHerrera/Assets/MoveRight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Threading;
+using UnityEngine;
+
+public class MoveRight : MonoBehaviour
+{
+    public Transform myTransform;
+
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myTransform.position = new Vector3(-10, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myTransform.position = myTransform.position + new Vector3(1, 0, 0) * speed * Time.deltaTime;
+    }
+}
