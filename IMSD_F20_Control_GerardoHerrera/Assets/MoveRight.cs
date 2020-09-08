f9dd9de5f6af81cb28ff3181696024cd0ad318d7
Gerardo Herrera <gerardohe17@gmail.com>
2020-09-08 16:46:26 -0700

    moveRight

--- IMSD_F20_Control_GerardoHerrera/Assets/MoveRight.cs
+++ IMSD_F20_Control_GerardoHerrera/Assets/MoveRight.cs
@@ -9,15 +9,24 @@ public class MoveRight : MonoBehaviour
     public Transform myTransform;
 
     public float speed;
+
+    private float timer;
     // Start is called before the first frame update
     void Start()
     {
         myTransform.position = new Vector3(-10, 0, 0);
+        timer = 0
     }
 
     // Update is called once per frame
     void Update()
     {
         myTransform.position = myTransform.position + new Vector3(1, 0, 0) * speed * Time.deltaTime;
+        timer += Time.deltaTime;
+        if (timer >= 1)
+        {
+            timer = 0;
+            myTransform.position += new Vector3(1, 0, 0);
+        }
     }
 }
