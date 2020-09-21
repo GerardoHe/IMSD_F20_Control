f24ed5125a994310ab4333440e48cd6c1327a8f4
Gerardo Herrera <gerardohe17@gmail.com>
2020-09-20 17:41:13 -0700

    circle

--- IMSD_F20_Control_GerardoHerrera/Assets/circlemove.cs
+++ IMSD_F20_Control_GerardoHerrera/Assets/circlemove.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class circlemove : MonoBehaviour
 {
+    Transform myTransform;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myTransform = transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.W))
+        {
+            myTransform.position += new Vector3(0, .1f, 0);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            myTransform.position += new Vector3(0, .1f, 0);
+        }
     }
 }
