df74175ae9e970a2c575539845f490e0120af8c5
Gerardo Herrera <gerardohe17@gmail.com>
2020-09-20 19:30:45 -0700

    frog

---
+++ IMSD_F20_Control_GerardoHerrera/Assets/frogmove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class frogmove : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    float speed = 1.25f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+            transform.position += Vector3.up * speed;
+        if (Input.GetKeyDown(KeyCode.S))
+            transform.position += Vector3.down * speed;
+    }
+}
---
+++ IMSD_F20_Control_GerardoHerrera/Assets/movelikecar.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class movelikecar : MonoBehaviour
+{
+    private Transform myTransform;
+    private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myTransform = transform;
+        speed = Random.Range(1.00f, 3.00f);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        myTransform.position += Vector3.right * Time.deltaTime * speed;
+    }
+}
